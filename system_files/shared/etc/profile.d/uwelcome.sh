e0d521709c73c54c9da931fd711e12e672935720
Mimolet <115139719+theMimolet@users.noreply.github.com>
2026-07-26 06:28:38 -0400

    chore: forgot to remove the `|| true` here

--- system_files/shared/etc/profile.d/uwelcome.sh
+++ system_files/shared/etc/profile.d/uwelcome.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ -e ~/.config/no-show-user-motd ]; then
-	mv ~/.config/no-show-user-motd ~/.config/uwelcome/disabled 2>/dev/null || true
+	mv ~/.config/no-show-user-motd ~/.config/uwelcome/disabled 2>/dev/null
 fi
 uwelcome
