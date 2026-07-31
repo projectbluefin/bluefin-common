86cc9a8d8b65cbfd99a87c96e7cab319d27a8802
Mimolet <115139719+theMimolet@users.noreply.github.com>
2026-07-31 09:32:27 -0400

    Update system_files/shared/usr/share/fish/vendor_conf.d/fish_greeting.fish
    Co-authored-by: James Reilly <jreilly1821@gmail.com>

--- system_files/shared/usr/share/fish/vendor_conf.d/fish_greeting.fish
+++ system_files/shared/usr/share/fish/vendor_conf.d/fish_greeting.fish
@@ -1,7 +1,8 @@
 function fish_greeting
 
     if test -e ~/.config/no-show-user-motd
-        mv ~/.config/no-show-user-motd ~/.config/uwelcome/disabled
+        mkdir -p ~/.config/uwelcome
+        mv ~/.config/no-show-user-motd ~/.config/uwelcome/disabled 2>/dev/null
     end
 
     uwelcome
